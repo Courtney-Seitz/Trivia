8b803cd8a9890d3eb6d8b2c07886b13034fd28b1
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-07 17:01:30 -0400

    setting up scoring, hopefully

--- trivia.js
+++ trivia.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 })
 
-// click start button (possibly seperate categories)
+// Psuedo-code:
+//click start button (possibly seperate categories)??
 //first question box appears, multiple choice (checkbox)
-//user clicks/selects answer, question box disappears, next question box appears...
-//score is tabulated on screen with progress bar?
-//timer function? (during game)?
-//end of quiz display score and time?
+//user clicks/selects answer and clicks 'next', question box disappears, next question box appears...
+//score is tabulated on screen with progress bar??
+//timer function? (during game)??
+//end of quiz display score and time??
 
 //Total number of questions
 var totalNumQuestions = $('.question').size();
@@ -23,13 +24,13 @@ $question.hide();
 //Show the first question
 $($question.get(currentQuestion)).fadeIn();
 
-//attach a click listener to the HTML element with the id of 'next'
+//Click listener to get next question...
 $('.next').click(function() {
 
-//current question disappears...
+//Current question disappears...
 $($question.get(currentQuestion)).fadeOut(function() {
 
-//questions go up one by one
+//Questions go up one by one
 currentQuestion = currentQuestion + 1;
 
 //if (currentQuestion == totalNumQuestions) {
@@ -38,7 +39,7 @@ currentQuestion = currentQuestion + 1;
 //}
 //else
 //{
-//otherwise show the next question
+//Otherwise show the next question
 $($question.get(currentQuestion)).fadeIn();
 
 });
