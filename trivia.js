b5746a6d70abf626145284ab942ebcf4c734fbf8
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-08 11:54:42 -0400

    more jquery testing

--- trivia.js
+++ trivia.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-})
+
+});
 
 //Psuedo-Code:
 //click start button (wishlist - possibly seperate categories)??
@@ -42,31 +43,15 @@ $($question.get(currentQuestion)).fadeIn();
 
 });
 
+
+
 //...Scoring...wants this in jQuery, eventually...
 
-function totalScore() {
+// var score = 0;
 
-var score = 0;
+// $(".results").on("click", function()) {
 
-var checkedAnswer;
 
-var answers = document.getElementsByClassName('.answers');
-
-var numberOfAnswers = answers.length;
-
-for(var i = 0; i < answers.length; i++)
-
-checkedAnswer = answers[i].options[answers[i].selectedIndex].value;
-
-if(checkedAnswer=== "correct") {
-
-  correct++;
-
-}
-
-document.getElementsByClassName("results").innerHTML = (100/"numberOfAnswers") * 'correct';
-
-}
 // });
 // });
 // });
