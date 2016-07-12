e741408c1835a95c10632f5e52d300b6dcc247b2
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-12 16:19:51 -0400

    upgrade

--- trivia.js
+++ trivia.js
@@ -1,66 +1,64 @@
-//Psuedo-Code:
-//click start button
-//wishlist - possibly separate categories
-//first question box appears, multiple choice
-//user clicks/selects answer and clicks 'next', question box disappears, next question box appears...
-//score is tabulated on screen
-//wishlist - progress bar
-//wishlist - timer function
-//wishlist - add play again button
-
 $(document).ready(function(){
-  $(".again").hide();
-  $(".questionContainer").hide();
-  $(".next").hide();
-  $(".scoreDisplay").hide();
+  console.log("Ready!");
+  //hides intro text, leaves 'start' button visible
+  $("#trivia").hide();
+  //reveals intro text on click event, calls function for running game, fill value of button with 'start game'
+  $("#startGame").on("click", beginGame); {
+    $("#startGame").val("Start Game");
+  }
+  //question array starts at zero/beginning
+  var currentQuestion = 0;
+  //score starts at zero
+  var score = 0;
+  //function for event listener, shows intro text, starts game
+  function beginGame() {
+    $("#trivia").fadeIn("slow");
+    if (currentQuestion < triviaQuestions.length) {
+      $("p.questions").html(triviaQuestions[currentQuestion].question);
+      //var selectionChoice = $("div.options").html(triviaQuestions[currentQuestion].selection);
+      var selections = triviaQuestions[currentQuestion].selection;
+      console.log(selections);
+      var radioSelection = $("div.options");
+      for (var i = 0; i < selections.length; i++) {
+        console.log("looping");
+        radioSelection.prepend('<label><input type="radio" name="radio_buttons" value="' + selections[i] +'"/>' + selections[i] + '</label>');
+
+      }
+      currentQuestion++;
+    }
+    //changes text of button to 'next question' as game begins
+    if (currentQuestion > 0 ) {
+      $("#startGame").val("Next Question");
+    }
+    //put in function here to have button show 'Play Again' text when quiz ends
+
+  }
+  //to-do: have selections show with radio buttons; have selections matched to correct or incorrect answers; show score; have button change to 'next question' and 'play again' at appropriate place within quiz;
+  //question/answer array of an array stored in a variable
+  var triviaQuestions = [
+
+    {
+      question: "Where did bananas originate?",
+      selection: ["Southeast Asia", "The Caribbean", "The Grocery Store", "Morocco" ],
+      correctSelection: 0
+    },
+    {
+      question: "How is the banana plant classified botanically?",
+      selection: ["Shrubbery", "Herb", "Tree", "Delicious"],
+      correctSelection: 1
+    },
+    {
+      question: "What do agriculturists call bananas growing together on a single stem?",
+      selection: ["Fingerlings", "Toes", "Hands", "Legs"],
+      correctSelection: 2
+    },
+    {
+      question: "Into which of the following sub-categories does the banana fruit best fit?",
+      selection: ["Nut", "Legume", "Sundae", "Berry"],
+      correctSelection: 3
+    },
+
+  ];
+
+
 });
-
-//Start function, unhide questions, other buttons
-$(".startGame").click(function() {
-  $(".questionContainer").show();
-  $(".next").show();
-  $(".scoreDisplay").show();
-});
-
-//Total number of questions
-var totalNumQuestions = $('.question').size();
-
-//Display current question, sets it at first question
-var currentQuestion = 0;
-
-//jQuery variable
-$question = $('.question');
-
-//Hide all of the questions
-$question.hide();
-
-//Show the first question
-$($question.get(currentQuestion)).fadeIn();
-
-//Click listener to get next question...
-$('.next').click(function() {
-
-//Current question disappears...
-  $($question.get(currentQuestion)).fadeOut(function() {
-
-//Questions go up one by one
-    currentQuestion = currentQuestion + 1;
-
-//Next question...
-    $($question.get(currentQuestion)).fadeIn();
-
-  });
-
-});
-
-//Scoring function
-function setScore() {
-  var score = $('input:checked[value=correct]').length
-  $('#score').text(score);
-}
-
-//Set score on input change (as user selects the correct answer)...
-$('input').change(setScore);
-
-//Set score when initialized...
-setScore();
