48802c1b1ab92dd001dd2597630cb1d24dbe3e9f
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-13 16:49:45 -0400

    working through scoring function

--- trivia.js
+++ trivia.js
@@ -4,18 +4,16 @@ $(document).ready(function(){
   var currentQuestion = 0;
   //score starts at zero
   var score = 0;
-
+  index = 0
   //hides intro text, leaves 'start' button visible
   $(".intro").hide();
   //reveals intro text on click event, calls function for running game, fill value of button with 'start game'
   $("#startGame").on("click", beginGame); {
     $("#startGame").val("Start Game");
   }
-
   //function for event listener, shows intro text, starts game
   function beginGame() {
     $(".intro").fadeIn("slow");
-    //score++;
     if (currentQuestion < triviaQuestions.length) {
       $("#startGame").val("Next Question")
       $("#questionBox").remove();
@@ -25,60 +23,58 @@ $(document).ready(function(){
       var selections = triviaQuestions[currentQuestion].selection;
       //  console.log(selections);
       for (var i = 0; i < selections.length; i++) {
-        console.log("looping");
-        $("#quizBox").append("<label><div class='optionsList'><input id='selectRadio' type='radio' name='" + selections[i] +"'/>" + selections[i] + '<br /></div></label>');
+        $("#quizBox").append("<label><div class='optionsList'><input class='selectRadio' type='radio' name='" + selections[i] +"'/>" + selections[i] + '<br /></div></label>');
       }
       $("#startGame").prepend("</p>");
+
+      $("div.optionsList").on("click", function() {
+        console.log("optionsList click event");
+        console.log($("div.optionsList").text());
+        console.log(currentQuestion);
+if ($("div.optionsList").html() === triviaQuestions[currentQuestion].correctSelection) {
+          score++;
+          console.log(score);
+       }
+      });
       currentQuestion++;
-      console.log("looping");
     }
     else {
       $("p.questions").remove();
       $("p.options").remove();
       $("#questionBox").remove();
       $(".optionsList").remove();
-
       $("#startGame").before('<h2 class="totalScore"> Final score: ' + score + ' / 4 questions corrrect.</h2>');
       $("#startGame").val("Play Again");
       //$("#startGame").remove();
       //$("#playAgain").show();
-      //score++;
-      //$("#selectRadio input[type='radio']:checked")
-      //console.log($("input[type='radio']:checked").val());
-      if (currentQuestion < triviaQuestions.length) {
-        if ($("input:checked").val() == triviaQuestions[currentQuestion].correctSelection) {
-          score++;
-        }
-      }
+
     }
-
-
   }
-  //put in function here to have button show 'Play Again' text when quiz ends
+
+
   //question/answer array of an array stored in a variable
   var triviaQuestions = [
     {
       question: "Where did bananas originate?",
-      selection: ["Southeast Asia", "The Caribbean", "The Grocery Store", "Morocco" ],
-      correctSelection: 0
+      selection: [" Southeast Asia ", " The Caribbean ", " The Grocery Store ", " Morocco " ],
+      correctSelection: "Southeast Asia"
     },
     {
       question: "How is the banana plant classified botanically?",
       selection: ["Shrubbery", "Herb", "Tree", "Delicious"],
-      correctSelection: 1
+      correctSelection: "Herb"
     },
     {
       question: "What do agriculturists call bananas growing together on a single stem?",
       selection: ["Fingerlings", "Toes", "Hands", "Legs"],
-      correctSelection: 2
+      correctSelection: "Hands"
     },
     {
       question: "Into which of the following sub-categories does the banana fruit best fit?",
       selection: ["Nut", "Legume", "Sundae", "Berry"],
-      correctSelection: 3
+      correctSelection: "Berry"
     },
 
   ];
-
 
 });
