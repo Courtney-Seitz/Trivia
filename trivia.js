1ee1f2a35dce4f816d870494d05a5e44b6782940
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-12 20:57:25 -0400

    drying up js

--- trivia.js
+++ trivia.js
@@ -1,40 +1,49 @@
 $(document).ready(function(){
   console.log("Ready!");
-  //hides intro text, leaves 'start' button visible
-  $(".intro").hide();
-  //reveals intro text on click event, calls function for running game, fill value of button with 'start game'
-  $("#startGame").on("click", beginGame); {
-    $("#startGame").val("Start Game");
-  }
   //question array starts at zero/beginning
   var currentQuestion = 0;
   //score starts at zero
   var score = 0;
   var index = 0;
+  var start = $("#startGame");
+  var radio = $("radio_buttons");
+  var radioSelection = $("div.options");
+  var intro = $(".intro");
+  var selections = triviaQuestions[currentQuestion].selection;
+  //hides intro text, leaves 'start' button visible
+  intro.hide();
+  //reveals intro text on click event, calls function for running game, fill value of button with 'start game'
+  start.on("click", beginGame); {
+  start.val("Start Game");
+  }
+
   //function for event listener, shows intro text, starts game
   function beginGame() {
-    $(".intro").fadeIn("slow");
+    intro.fadeIn("slow");
     if (currentQuestion < triviaQuestions.length) {
-      $("#startGame").val("Next Question")
+      start.val("Next Question")
       $("p.questions").html(triviaQuestions[currentQuestion].question);
       //var selectionChoice = $("div.options").html(triviaQuestions[currentQuestion].selection);
-      var selections = triviaQuestions[currentQuestion].selection;
+
       //console.log(selections);
-      var radioSelection = $("div.options");
       for (var i = 0; i < selections.length; i++) {
-        console.log("looping");
-        radioSelection.after('<div class="optionsList"><label><input type="radio" id="radio_buttons" value="' + selections[i] +'"/>' + selections[i] + '</label></div>');
-//if (currentQuestion < triviaQuestions.length) {
-//  $("div.optionsList").remove();
-//}
+        radioSelection.html('<div class="optionsList"><label><input type="radio" id="radio_buttons" value="' + selections[i] +'"/>' + selections[i] + '</label></div>');
       }
-      currentQuestion++;
+if (currentQuestion == triviaQuestions.length - 1) {
+start.val("Submit");
+} else if (currentQuestion > triviaQuestions.length - 1) {
+  console.log("I'm a placeholder for scoring function")
+}
+
+
+//      currentQuestion++;
     }
 
     //put in function here to have button show 'Play Again' text when quiz ends
 
   }
-  //to-do: have selections show with radio buttons; have selections matched to correct or incorrect answers; show score; have button change to 'next question' and 'play again' at appropriate place within quiz;
+
+
   //question/answer array of an array stored in a variable
   var triviaQuestions = [
 
